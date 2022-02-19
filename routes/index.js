0935b0d7070b01a5ec3055c96c19c656e003d1bd
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-19 14:33:27 -0500

    Initial Setup

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const storeController = require('../controllers/storeController')
+const router = express.Router();
+
+// Do work here
+router.get('/', storeController.homePage);
+
+module.exports = router;
