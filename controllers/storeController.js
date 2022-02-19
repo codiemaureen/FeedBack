025e36e4edd7e88b2f830eea68ecc9b608e7f7f5
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-19 15:45:25 -0500

    Querying all stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,5 +14,10 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
     const store = await new Store(req.body).save();
     req.flash('success', `You have successfully created ${store.name}`);
-    res.redirect('/');
+    res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find({});
+    res.render('stores', {title: 'Stores', stores});
+}
