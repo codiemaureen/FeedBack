c3524a6dbb8daa673778120faa3f59333b06698d
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-19 15:25:49 -0500

    Adding New Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,18 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
     res.render('store', {title: 'homePage'});
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add Store'});
+};
+
+
+
+exports.createStore = async (req, res) => {
+    const store = await new Store(req.body).save();
+    req.flash('success', `You have successfully created ${store.name}`);
+    res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const storeController = require('../controllers/storeController')
 const router = express.Router();
+const {catchErrors} = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
