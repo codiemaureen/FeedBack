1ba72a28b93ecc130363c172ba91d384127f9b53
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-20 18:39:49 -0500

    Adding Top Stores

--- routes/index.js
+++ routes/index.js
@@ -50,6 +50,7 @@ router.post('/reviews/:id',
     authController.isLoggedIn, 
     catchErrors(reviewController.addReview))
 
+router.get('/top', catchErrors(storeController.getTopStores));
 
 // API ROUTES //
 
