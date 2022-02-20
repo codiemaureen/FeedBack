e9b3a04066790806c5593800af779d5b66073555
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-20 01:47:48 -0500

    Enabiling Password Reset Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
@@ -38,7 +39,13 @@ exports.forgot = async(req, res) =>{
     await user.save();
 
     const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
-    req.flash('success', `you have been sent a password link. ${resetURL}`);
+    mail.send({
+        user,
+        subject: 'Password Reset',
+        resetURL,
+        filename: 'password-reset'
+    })
+    req.flash('success', `you have been sent a password link.`);
     res.redirect('/login');
 };
 
